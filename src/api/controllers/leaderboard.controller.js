9c569eabdd4b4a0a6103040a5a3b5b02730339ba
Czekaj Tom <red57101@gmail.com>
2023-12-08 06:04:13 +0100

    :sparkles: Add leaderboard

--- src/api/controllers/leaderboard.controller.js
+++ src/api/controllers/leaderboard.controller.js
@@ -1,8 +1,20 @@
 /* eslint-disable no-unused-vars */
-const {User} = require("@database/database");
+const {User, Game} = require("@database/database");
+const {computeGameScore} = require("./game.controller");
 
 async function getLeaderboard(req, res){
     const users = await User.findAll();
+    const userLowerScores = [];
+    for(const user of users){
+        const userGames = await Game.findAll();
+        const userScores = [];
+        for(const userGame of userGames)
+            userScores.push(await computeGameScore(userGame.id));
+        if(userScores.length > 0)
+            userLowerScores.push({user: user, score: Math.min(...userScores)});
+    }
+    userLowerScores.sort((a, b) => b.score - a.score);
+    return res.status(200).json({leaderboard: userLowerScores});
 }
 
 module.exports = {
