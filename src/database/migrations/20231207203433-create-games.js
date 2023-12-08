2cb5b2d69fae2c24bc33a3fb19466a7d4ffb847a
Czekaj Tom <red57101@gmail.com>
2023-12-08 01:57:43 +0100

    :bug: Fix wrong migration

--- src/database/migrations/20231207203433-create-games.js
+++ src/database/migrations/20231207203433-create-games.js
@@ -37,6 +37,6 @@ module.exports = {
         });
     },
     async down(queryInterface, Sequelize){
-        await queryInterface.dropTable("Games");
+        await queryInterface.dropTable("games");
     }
 };
