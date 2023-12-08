f0c39f9c43382dc7a65260e3acf5d825394d9005
Loïc MAES <97891718+loicmaes@users.noreply.github.com>
2023-12-08 05:53:39 +0000

    :rocket: Deploy seeds

--- src/database/seeders/20231207213141-quiz-data.js
+++ src/database/seeders/20231207213141-quiz-data.js
@@ -4,36 +4,48 @@ module.exports = {
         await queryInterface.bulkInsert("quiz_data", [
             {
                 id: 1,
-                image: "https://images.unsplash.com/photo-1606326608606-aa0b62935f2b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                question: "What is the capital of France?",
-                right_answer: "Paris",
-                wrong_answer_1: "Marseille",
-                wrong_answer_2: "Lyon",
-                wrong_answer_3: "Toulouse",
+                image: "https://images.unsplash.com/photo-1474487548417-781cb71495f3?q=80&w=2484&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                question: "Quand je dois me déplacer, je dois privilégier :",
+                right_answer: "Les transports en commun",
+                wrong_answer_1: "Ma moto",
+                wrong_answer_2: "Ma voiture",
+                wrong_answer_3: "Mon jet privé",
                 created_at: new Date(),
-                updated_at: new Date(),
-            },
-            {
+                updated_at: new Date()
+              },
+              {
                 id: 2,
-                question: "What is the capital of Spain?",
-                right_answer: "Madrid",
-                wrong_answer_1: "Barcelona",
-                wrong_answer_2: "Seville",
-                wrong_answer_3: "Valencia",
+                image: "",
+                question: "Pour réduire ma consommation électrique, je peux :",
+                right_answer: "Utiliser des équipements économes en énergie",
+                wrong_answer_1: "Chauffer tout l'été pour ne pas avoir à chauffer l'hiver",
+                wrong_answer_2: "Chauffer toute l'année",
+                wrong_answer_3: "Rester les fenêtres ouvertes l'hiver",
                 created_at: new Date(),
-                updated_at: new Date(),
-            },
-            {
+                updated_at: new Date()
+              },
+              {
                 id: 3,
-                image: "https://plus.unsplash.com/premium_photo-1681248156511-200ffb3b66cc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                question: "What is the capital of Germany?",
-                right_answer: "Berlin",
-                wrong_answer_1: "Hamburg",
-                wrong_answer_2: "Munich",
-                wrong_answer_3: "Cologne",
+                image: "",
+                question: "Si je veux chauffer, je dois privilégier :",
+                right_answer: "Le biogaz",
+                wrong_answer_1: "Le pétrol",
+                wrong_answer_2: "Le charbon",
+                wrong_answer_3: "Le gaz",
                 created_at: new Date(),
-                updated_at: new Date(),
-            }
+                updated_at: new Date()
+              },
+              {
+                id: 4,
+                image: "",
+                question: "En collectivité, je peux :",
+                right_answer: "Partager mon véhicule pour des trajets en commun",
+                wrong_answer_1: "Sauter dans le premier avion avec mon voisin",
+                wrong_answer_2: "Emmener mon collègue au boulot en hélicoptère",
+                wrong_answer_3: "Jouer de la flute pour mes chiens, à l'arrêt, le moteur allumé",
+                created_at: new Date(),
+                updated_at: new Date()
+              }
         ]);
     },
 
